63f9e7ad38e81be4823ed1244d090f4ebc8cf51b
steveechan <chanstephen2121@gmail.com>
2023-04-15 12:54:11 -0400

    added color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sage: {
+          main: "#3D5A4F",
+          accent1: "#8BC4A4",
+          accent2: "#B6D3BB",
+          accent3: "#F0F0E8",
+          dark: "#1E1E1E",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
