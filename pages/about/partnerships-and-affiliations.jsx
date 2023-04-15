1cecca29635bfe70bad01247a79c18bf67cdbfc2
steveechan <chanstephen2121@gmail.com>
2023-04-15 16:54:51 -0400

    update text

--- pages/about/partnerships-and-affiliations.jsx
+++ pages/about/partnerships-and-affiliations.jsx
@@ -8,9 +8,9 @@ const PartnershipsAndAffiliations = () => {
         <div className="max-w-4xl space-y-8 mx-auto p-4">
        <div>
          <p className="text-sage-accent1">Partnerships and Affiliations</p>
-        <h1 className="2xl:text-5xl xl:text-5xl lg:text-5xl md:text-5xl sm:text-5xl text-3xl 2xl:leading-relaxed xl:leading-relaxed lg:leading-relaxed md:leading-relaxed sm:leading-relaxed leading-snug">Our Valued Partners and Affiliates: A Strong Network of Trusted Experts Who Share Our Vision for Agile and Lean Excellence</h1>
+        <h1 className="2xl:text-5xl xl:text-5xl lg:text-5xl md:text-5xl sm:text-5xl text-3xl 2xl:leading-relaxed xl:leading-relaxed lg:leading-relaxed md:leading-relaxed sm:leading-relaxed leading-snug">A Strong Network of Trusted Experts Who Share Our Vision for Agile and Lean Excellence</h1>
        </div>
-        <p className="leading-loose">Our partnerships and affiliations are a vital part of our commitment to providing top-notch agile and lean solutions. We are proud to work with a network of trusted experts who share our values and help us deliver exceptional services to our clients.</p>
+        <p className="leading-loose">Our partnerships and affiliations are a vital part of our commitment to providing top-notch agile and lean solutions. We are proud to work with a network of trusted experts who share our values and help us deliver exceptional services to our students.</p>
          <div className="mt-8 ">
           <button className="flex items-center gap-2 text-sm  bg-sage-main px-8 py-4 rounded-full">
                 Join our network <AiOutlineSwapRight />{" "}
