b8ce81d7d2e4828d15516e461efe5b13c3b65e24
steveechan <chanstephen2121@gmail.com>
2023-04-29 22:42:28 -0400

    added style

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,12 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { MetaHeader } from "../components/common/metaHeader";
 
 
 const ErrorPage = () => {
 
     return <main className="min-h-[80vh] grid place-items-center">
-
-        <h1 className="text-center">404 | Something went wrong</h1>
+        <MetaHeader title="404 | Page not found" description="Oops! The page you are looking for could not be found. It may have been moved, deleted, or never existed. Please check the URL or use the search function to find what you're looking for." />
+        <h1 className="text-center max-w-4xl text-4xl leading-normal">404 | Oops! The page you are looking for could not be found. </h1>
     </main>
 }
 export default ErrorPage;
--- pages/500.js
+++ pages/500.js
@@ -1,11 +1,12 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { MetaHeader } from "../components/common/metaHeader";
 
 
 const ErrorPage = () => {
 
     return <main className="min-h-[80vh] grid place-items-center">
-
-        <h1 className="text-center">500 | Something went wrong</h1>
+        <MetaHeader title="500 | Internal Server Error" description="Oops! Something went wrong. The server encountered an internal error and was unable to complete your request. Please try again later or contact the website administrator for assistance." />
+        <h1 className="text-center max-w-4xl text-4xl leading-normal" >500 |  Internal Server Error</h1>
     </main>
 }
 export default ErrorPage;
